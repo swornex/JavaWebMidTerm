3cd4bac4902f1796db98cbfdac4330b872bc6cd3
Swornima <sworstha@gmail.com>
2022-02-23 01:32:11 +0545

    Modified Folders

--- Question-One-Login/src/main/java/org/iims/dao/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package org.iims.dao;
-
-import org.iims.model.Login;
-
-import java.sql.SQLException;
-
-public interface LoginDao {
-    Login findOne(String username, String password) throws SQLException, ClassNotFoundException;
-}
